bea34dba002ca0d2c175e0b4621ed5c2f05478c4
FabioMiranda-123 <fabio_rebelo@outlook.pt>
2025-05-03 16:00:42 +0100

    Navbar Style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 import { Button } from "./Button";
-
 export function NavBar() {
   return (
     <nav className="fixed top-0 left-0 w-full flex justify-between items-center px-4 py-2 bg-white shadow-navbarshadow z-50 md:px-8 lg:px-16">
-      <h1 className="text-darkgreen text-h3">🌱 Plant Pal</h1>
+      <Link to="/">
+        <h1 className="text-darkgreen text-h3">🌱 Plant Pal</h1>
+      </Link>
       <Link to="/AddPlant">
         <Button text="Add Plant" />
       </Link>
