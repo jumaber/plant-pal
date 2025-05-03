538aacb2310c15a824ba74c5321085899007dcbf
Julia Mari Bernaus <jumaber@gmail.com>
2025-05-03 17:06:23 +0200

    comments on how to use the root colors

--- src/components/Button_Back.jsx
+++ src/components/Button_Back.jsx
@@ -4,7 +4,7 @@ export function ButtonBack() {
   return (
     <Link
       to="/"
-      className="flex flex-row justify-start items-center text-lg font-bold text-darkgreen bg-lightgreen w-full py-4 px-4 md:px-8 lg:px-16 "
+      className="flex flex-row justify-start items-center text-lg font-bold text-darkgreen w-full py-4 px-4 md:px-8 lg:px-16 "
     >
       ← Back
     </Link>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,11 @@ import { ButtonBack } from "../components/Button_Back"
 import { NavBar } from "../components/Navbar"
 
 export function HomePage (){
-    return(
-        <div className="">
-            <NavBar />
-            <h2>My Plants</h2>
-            <ButtonBack />
-            
-        </div>
-    )
+    return (
+      <div className="bg-[var(--color-background)]">
+        <NavBar />
+        <h2>My Plants</h2>
+        <ButtonBack />
+      </div>
+    );
 }
