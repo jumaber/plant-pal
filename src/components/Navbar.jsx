bfd3a52ae4e92990975ff5d654c85ff02e5b91cb
FabioMiranda-123 <fabio_rebelo@outlook.pt>
2025-05-03 16:00:42 +0100

    Navbar Style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
-import { Link } from "react-router-dom"
-import { ButtonLarge } from "./Button_Large"
-
-export function NavBar(){
-    return (
-        <nav>
-            <h1>Plant Pal</h1>
-            <Link to="/add">
-            <ButtonLarge text="Add Plant" />
-        </Link>
-        </nav>
-    )
+import { Link } from "react-router-dom";
+import { Button } from "./Button";
+export function NavBar() {
+  return (
+    <nav className="fixed top-0 left-0 w-full flex justify-between items-center px-4 py-2 bg-white shadow-navbarshadow z-50 md:px-8 lg:px-16">
+      <Link to="/">
+        <h1 className="text-darkgreen text-h3">:seedling: Plant Pal</h1>
+      </Link>
+      <Link to="/AddPlant">
+        <Button text="Add Plant" />
+      </Link>
+    </nav>
+  );
 }
