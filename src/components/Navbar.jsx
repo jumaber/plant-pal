b0b6f1e24a4f7f95ede1c49192db8070862dcacb
Julia Mari Bernaus <jumaber@gmail.com>
2025-05-06 19:38:34 +0200

    added emoticon and font to logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,7 @@ import { Button } from "./Button";
 export function NavBar() {
   return (
     <nav className="fixed top-0 left-0 w-full flex justify-between items-center px-4 py-2 bg-white shadow-navbarshadow z-50 md:px-8 lg:px-16">
-      <Link to="/">
-        <h1 className="text-darkgreen text-h3">:seedling: Plant Pal</h1>
-      </Link>
+      <h1 className="text-darkgreen">Plant Pal</h1>
       <Link to="/AddPlant">
         <Button text="Add Plant" />
       </Link>
