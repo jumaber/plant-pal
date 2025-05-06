2687c7f3eadcad62886e97159e00b5f3470b4737
Julia Mari Bernaus <jumaber@gmail.com>
2025-05-06 21:39:57 +0200

    styled Home Page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ import { NavBar } from "../components/Navbar"
 
 export function HomePage (){
     return (
-      <div className="flex flex-col gap-8 bg-[var(--color-background)] p-4 md:px-8 lg:px-20">
+      <div className="flex flex-col gap-8 bg-[var(--color-background)] px-4  py-24 lg:py–30 md:px-8 lg:px-20">
         <NavBar />
         <div className="text-h1">My Plants</div>
         <PlantList />
