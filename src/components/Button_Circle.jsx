57d6f87cce2e6264ec2f95a4f345d128cb98a3a5
Julia Mari Bernaus <jumaber@gmail.com>
2025-05-06 19:36:02 +0200

    Plant Card beginning

--- src/components/Button_Circle.jsx
+++ src/components/Button_Circle.jsx
@@ -1,14 +1,12 @@
 export function ButtonCircle({
   text = "💦",
   bgColor = "bg-[var(--color-background)]",
-  onClick,
-  isWatered = false, // ✅ new prop from parent
 }) {
   return (
     <button
       onClick={onClick}
       className={`flex w-14 h-14 justify-center items-center radius-circular text-center text-h3 font-bold py-[10px] ${
-        isWatered ? "bg-[var(--color-lightgreen)]" : bgColor
+        clicked ? "bg-[var(--color-lightgreen)]" : bgColor
       }`}
     >
       {isWatered ? "✅" : text}
